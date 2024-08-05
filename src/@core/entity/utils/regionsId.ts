12e2808cfdd3d82560b22ec4b269afc7f3da6501
Muhammad <muxammad_205@mail.ru>
2024-08-05 09:31:31 +0500

    extanded from mahalla existing project

---
+++ src/@core/entity/utils/regionsId.ts
@@ -0,0 +1,16 @@
+export const regionsId = {
+  Toshkent_shahar: "467f1fef-5872-4118-a3eb-4a971c69ce57",
+  Toshkent_viloyati: "4a92aa0c-edf6-4963-aa8a-f1ae98b90f7e",
+  Namangan_viloyati: "57170b63-d2a8-4f46-8ace-50a078962d63",
+  Andijon_viloyati: "9a48efd9-638f-43cd-9ca7-7cbafed5b8a7",
+  Sirdaryo_viloyati: "c15e4300-5c68-4550-948c-4abe5e834af1",
+  Qoraqalpogiston_respublikasi: "438214b7-1ccc-4216-92ac-3474ee74e5bf",
+  Fargona_viloyati: "558c3472-fa08-456d-9e00-5fed45f5cc86",
+  Xorazm_viloyati: "7d8c72d8-cf5e-4ea7-ad04-7fe8ec5d8f8c",
+  Jizzax_viloyati: "619a28d5-09d7-4f83-909e-93c39b050ad4",
+  Surxondaryo_viloyati: "c1a7a3ad-1906-4ae6-a607-c65ab74a1f4a",
+  Samarqand_viloyati: "b6839b90-b1a1-4c83-bb1d-03c3382c498c",
+  Navoiy_viloyati: "3c910446-531e-4787-b793-95a02672730b",
+  Qashqadaryo_viloyati: "c4b8cd67-52b2-4574-ad88-4c398b05a681",
+  Buxoro_viloyati: "bea1fba9-68ef-4b10-8de5-2e270290c2c5",
+};
---
+++ src/@core/pages/Control/Regions/api/index.ts
@@ -0,0 +1,24 @@
+import { api } from "@/@core/application/utils/api";
+
+export const get = async (params: any) => {
+  try {
+    const response = await api.get("/RegionCategories/all", {
+      params,
+    });
+
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// UPDATE
+export const update = async (values: unknown, id: number) => {
+  try {
+    const response = await api.patch(`/RegionCategories/update/${id}`, values);
+
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+};
